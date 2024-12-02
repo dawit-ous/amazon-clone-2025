ab32ebdf571bfc77bbcfa551fdc645cc3d4779d7
dawit-ous <dawit.ousman@gmail.com>
2024-12-02 02:41:59 -0500

    amazon-clone -with deployed-api

--- src/Components/Api/axios.js
+++ src/Components/Api/axios.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //local instance of firebase functions
+  
   baseURL: "http://127.0.0.1:5001/clone-3ed40/us-central1/api",
   //deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-siz4.onrender.com",
+
   // baseURL: "http://localhost:5000",
 });
 export { axiosInstance };
